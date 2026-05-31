de047dad53fc6b5d150a0a0f5e10f09e7c5c219c
Jonathon Robel <jonrrobel@gmail.com>
2026-05-31 17:36:54 -0600

    Init commit, hello world prgm

---
+++ hello.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, world!".
+           STOP RUN.
+           
